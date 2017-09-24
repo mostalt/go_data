6ade19aa4de5365655c52e6b83ce13bcc0bf4eb5
Sergey Antonov <mostalt.mail@gmail.com>
2017-09-24 17:00:18 +0700

    add simple map

--- main.go
+++ main.go
@@ -4,36 +4,11 @@ import (
 	"fmt"
 )
 
-type contactInfo struct {
-	email   string
-	zipCode int
-}
-
-type person struct {
-	firstName string
-	lastName  string
-	contact   contactInfo
-}
-
 func main() {
-	kolya := person{
-		firstName: "Kolya",
-		lastName:  "Shirshov",
-		contact: contactInfo{
-			email:   "ololo@gmail.com",
-			zipCode: 405405,
-		},
+	colors := map[string]string{
+		"black": "#000",
+		"white": "#fff",
 	}
 
-	kolyaPointer := &kolya
-	kolyaPointer.updateName("Kolyasha")
-	kolya.print()
-}
-
-func (pointerToPerson *person) updateName(newFirstName string) {
-	(*pointerToPerson).firstName = newFirstName
-}
-
-func (p person) print() {
-	fmt.Printf("%+v", p)
+	fmt.Println(colors)
 }
