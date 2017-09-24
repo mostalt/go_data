dc23ea3bb7cdf12e5b37f9203878ab67745ef956
Sergey Antonov <mostalt.mail@gmail.com>
2017-09-24 15:26:07 +0700

    struct with receiver function

--- main.go
+++ main.go
@@ -25,5 +25,14 @@ func main() {
 		},
 	}
 
-	fmt.Printf("%+v", kolya)
+	kolya.updateName("Kolusha")
+	kolya.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
